c005e2490def9eb8f4f728777bebad64a2940f5d
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-11-27 12:39:53 -0300

    agregando lenguajes

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,7 +3,7 @@
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="p-2 bg-gray-700 border-l-4 border-red-600">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
