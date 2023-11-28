cecb53de5cc7a061b204a1b48f82f9637daec463
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-11-28 19:09:19 -0300

    email veerify

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -15,12 +18,15 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         //
     ];
-
-    /**
-     * Register any authentication / authorization services.
-     */
+    
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificar Cuenta')
+                ->line('Tu cuenta esta casi lista, solo debes hacer click en el siguiente enlace.')
+                ->action('Confirmar Cuenta', $url)
+                ->line('Si no solicitaste este Email, puedes ignorar este mensaje.');
+        });
     }
 }
